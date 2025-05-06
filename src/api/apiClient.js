e905c3e96e24a2fb720ac4a244dd39058782d30a
penkayone <An.goloskokov@gmail.com>
2025-05-06 23:18:42 +0300

    Worked on the keys (temporary).

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { getToken } from '../services/auth';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://localhost:8080/api',
 });
 
 // Добавляем токен ко всем запросам, если пользователь авторизован
